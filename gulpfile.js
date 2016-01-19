e2a33fb741eb68a3da57584e01e429f330e89008
jfchen <jfchen@dataman-inc.com>
2016-01-19 19:38:07 +0800

    add feature: add glulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,10 @@ gulp.task('copy-fonts', ['copy-pics'], function() {
 gulp.task('html-replace', function() {
     var assets = useref.assets();
     var revAll = new RevAll({dontRenameFile: ['.html'], dontUpdateReference: ['.html']});
-    return gulp.src(['index.html', 'price.html', 'case-miaosha.html', 'company-app.html', 'wordpress.html'])
+    return gulp.src(['index.html', 'price.html', 'case-miaosha.html', 'company-app.html', 'companyapp-detail-discuz.html',
+                     'companyapp-detail-ecshop.html', 'companyapp-detail-elasticsearch.html', 'companyapp-detail-hadoop.html',
+                     'companyapp-detail-jekins.html', 'companyapp-detail-joomla.html', 'companyapp-detail-kafka.html',
+                     'companyapp-detail-lnmp.html', 'companyapp-detail-spark.html', 'companyapp-detail-wordpress.html'])
         .pipe(assets)
         .pipe(gulpif('*.js', uglify()))
         .pipe(gulpif('*.css', minifyCss()))
