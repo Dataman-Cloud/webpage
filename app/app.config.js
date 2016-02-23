93dc85d4efb7e273a54c0e3152daa8a2c3943130
my9074 <313231068@qq.com>
2016-02-23 18:36:27 +0800

    Add  for config.js

--- app/app.config.js
+++ app/app.config.js
@@ -1,75 +1,76 @@
-(function() {
-  'use strict';
-  angular.module('webpage')
-    .config(configure);
+(function () {
+    'use strict';
+    angular.module('webpage')
+        .config(configure);
 
-  configure.$inject = [
-    '$urlRouterProvider',
-    '$stateProvider',
-    '$locationProvider'
-  ];
+    configure.$inject = [
+        '$urlRouterProvider',
+        '$stateProvider',
+        '$locationProvider',
+        '$interpolateProvider'
+    ];
 
-  function configure(
-    $urlRouterProvider,
-    $stateProvider,
-    $locationProvider
-  ) {
+    function configure($urlRouterProvider,
+                       $stateProvider,
+                       $locationProvider, $interpolateProvider) {
 
-    $stateProvider
-        .state("register", {
-            url: '/user/register',
-            views: {
-                '': {
-                    templateUrl: '/components/register/register.html',
-                    controller: 'RegisterCtrl as registerCtrl'
+        $stateProvider
+            .state("register", {
+                url: '/user/register',
+                views: {
+                    '': {
+                        templateUrl: '/components/register/register.html',
+                        controller: 'RegisterCtrl as registerCtrl'
+                    }
                 }
-            }
-        })
-        .state("active", {
-            url: '/user/active',
-            views: {
-                '': {
-                    templateUrl: 'components/active/active.html',
-                    controller: 'ActiveCtrl as activeCtrl'
+            })
+            .state("active", {
+                url: '/user/active',
+                views: {
+                    '': {
+                        templateUrl: 'components/active/active.html',
+                        controller: 'ActiveCtrl as activeCtrl'
+                    }
                 }
-            }
-        })
-        .state('login', {
-            url: '/user/login',
-            views: {
-                '': {
-                    templateUrl: '/components/login/login.html',
-                    controller: 'LoginCtrl as loginCtrl'
+            })
+            .state('login', {
+                url: '/user/login',
+                views: {
+                    '': {
+                        templateUrl: '/components/login/login.html',
+                        controller: 'LoginCtrl as loginCtrl'
+                    }
                 }
-            }
-        })
-        .state('forgotpassword', {
-            url: '/user/forgotpassword',
-            views: {
-                '': {
-                    templateUrl: '/components/password/forgot-password.html',
-                    controller: 'ForgotPasswordCtrl as forgotPasswordCtrl'
+            })
+            .state('forgotpassword', {
+                url: '/user/forgotpassword',
+                views: {
+                    '': {
+                        templateUrl: '/components/password/forgot-password.html',
+                        controller: 'ForgotPasswordCtrl as forgotPasswordCtrl'
+                    }
                 }
-            }
-        })
-        .state('resetpassword', {
-            url: '/user/resetpassword',
-            views: {
-                '': {
-                    templateUrl: '/components/password/reset-password.html',
-                    controller: 'ResetPasswordCtrl as resetPasswordCtrl'
+            })
+            .state('resetpassword', {
+                url: '/user/resetpassword',
+                views: {
+                    '': {
+                        templateUrl: '/components/password/reset-password.html',
+                        controller: 'ResetPasswordCtrl as resetPasswordCtrl'
+                    }
                 }
-            }
-        })
-        .state('error', {
-            views: {
-                '': {
-                    templateUrl: '/common_views/error.html'
+            })
+            .state('error', {
+                views: {
+                    '': {
+                        templateUrl: '/common_views/error.html'
+                    }
                 }
-            }
-        });
+            });
 
 
-    $locationProvider.html5Mode(true);
-  }
+        $locationProvider.html5Mode(true);
+        $interpolateProvider.startSymbol('{/');
+        $interpolateProvider.endSymbol('/}');
+    }
 })();
