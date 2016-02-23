37ce1dbb3daae00399d6aa6c437bcaf6d45490b2
ainesmile <ainesmile@gmail.com>
2016-02-23 10:57:45 +0800

    add ui-route configs

--- app/app.config.js
+++ app/app.config.js
@@ -15,6 +15,56 @@
     $locationProvider
   ) {
 
+    $stateProvider
+        .state("register", {
+            url: '/user/register',
+            views: {
+                "": {
+                    templateUrl: '/components/register/register.html'
+                }
+            }
+        })
+        .state("active", {
+            url: '/user/active',
+            views: {
+                '': {
+                    templateUrl: 'components/active/active.html'
+                }
+            }
+        })
+        .state('login', {
+            url: '/user/login',
+            views: {
+                "": {
+                    templateUrl: '/components/login/login.html'
+                }
+            }
+        })
+        .state('forgotpassword', {
+            url: '/user/forgotpassword',
+            views: {
+                '': {
+                    templateUrl: '/components/password/forgot-password.html'
+                }
+            }
+        })
+        .state('resetpassword', {
+            url: '/user/resetpassword',
+            views: {
+                '': {
+                    templateUrl: '/components/password/reset-password.html'
+                }
+            }
+        })
+        .state('error', {
+            views: {
+                '': {
+                    templateUrl: '/common_views/error.html'
+                }
+            }
+        });
+
+
     $locationProvider.html5Mode(true);
   }
 })();
