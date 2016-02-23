9406a13018ea11e4f402ccc541873f6960f55b68
ainesmile <ainesmile@gmail.com>
2016-02-23 14:01:39 +0800

    modify conf.js to adapt function `getUrlTemplate`

--- conf.js
+++ conf.js
@@ -1,15 +1,12 @@
 CONFIG = {
     environment: 'ENVIRONMENT', //dev, demo, prod
     is_offline: OFFLINE, //set true or false
-    backendUrlBase: {
-        user: {
-            userBase: 'APIURL/',
-            dashboard: 'DASHBOARD/',
-            domain: 'BODY_DOMAIN',
-            demoUser: 'DEMO_USER'
-        },
-        version: {
-            base: 'MARKET' + '/version/',
-        }
-    }
+    dashboard: 'DASHBOARD/',
+    domain: 'BODY_DOMAIN',
+    demoUser: 'DEMO_USER'
+};
+
+BACKEND_URL_BASE = {
+    user: 'APIURL/',
+    version: 'MARKET' + '/version/'
 };
