c593344fc0d686cbfd3876a6dfddc81be91a248e
ainesmile <ainesmile@gmail.com>
2016-02-29 19:43:24 +0800

    go to top when state changes

--- root.ctrl.js
+++ root.ctrl.js
@@ -3,9 +3,9 @@
     angular.module('webpage')
         .controller('RootCtrl', RootCtrl);
 
-    RootCtrl.$inject = ['loginService', '$window'];
+    RootCtrl.$inject = ['loginService', '$window', '$rootScope'];
 
-    function RootCtrl(loginService, $window) {
+    function RootCtrl(loginService, $window, $rootScope) {
         var self = this;
 
         self.loginAsDemoUser = function () {
@@ -15,6 +15,10 @@
         self.goBack = function() {
             $window.history.back();
         };
+
+        $rootScope.$on('$stateChangeSuccess',function(){
+            $("html, body").animate({ scrollTop: 0 }, 200);
+        });
 
     }
 })();
