59e6a6ba8c43f740b74071ab3e4d8356e92b80e6
jfchen <jfchen@dataman-inc.com>
2016-07-01 16:05:00 +0800

    feature: remove no use config

--- conf.js
+++ conf.js
@@ -1,13 +1,9 @@
 CONFIG = {
     environment: 'ENVIRONMENT', //dev, demo, prod
-    is_offline: OFFLINE, //set true or false
     dashboard: 'DASHBOARD/',
-    domain: 'BODY_DOMAIN',
-    demoUser: 'DEMO_USER'
 };
 
 BACKEND_URL_BASE = {
-    defaultBase: 'APIURL/',
     version: 'MARKET' + '/version/' + CONFIG.environment + "-"
 };
 
