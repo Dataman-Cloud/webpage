34d211efbefdd61aa58de15c0f95cea5e8fe70e7
devops <devops@dataman-inc.com>
2017-02-03 16:51:18 +0800

    fix centos docker build bug

--- compress.sh
+++ compress.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 rm -rf build/*
 npm config set registry https://registry.npm.taobao.org
-npm install --global gulp
+#npm install --global gulp
 npm install
 gulp build && gulp && gulp clean
