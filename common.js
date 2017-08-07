0c529be40ac347746b71e5a044cadf9a2d73b622
lixiaoyanqq <lixy_9791@163.com>
2017-08-07 20:32:36 +0800

    Enterprise edition a drop-down menu

--- common.js
+++ common.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     $(function (window) {
         var cover = document.createElement('div');
         cover.className = 'cover';
@@ -9,8 +9,8 @@
         var nav = document.querySelector(".nav");
         var body = document.querySelector('body');
         var scroll = document.querySelector('.scroll');
-        btn.addEventListener('click', function() {
-        		scroll.style.display = 'block';
+        btn.addEventListener('click', function () {
+            scroll.style.display = 'block';
             var isDisplay = getCSSValue(cover, 'display')
             if (isDisplay == 'none') {
                 cover.style.display = 'block';
@@ -18,15 +18,15 @@
 //              $("body,html").css({"overflow":"hidden"});
             } else {
                 cover.style.display = 'none';
-               	nav.style.display = 'none';
-               	scroll.style.display = 'none';
+                nav.style.display = 'none';
+                scroll.style.display = 'none';
 //              $("body,html").css({"overflow":"visible"});
             }
         });
-        cover.addEventListener('click', function() {
+        cover.addEventListener('click', function () {
             cover.style.display = "none"
             nav.style.display = 'none';
-            $("body,html").css({"overflow":"visible"});
+            $("body,html").css({"overflow": "visible"});
         });
 
         function getCSSValue(obj, key) { //获取元素CSS值
@@ -38,44 +38,58 @@
         }
 
         // nav hover
-        if(isPC()) {
-        $('#dropdown').on("mouseover", function () {
-            $('#dropdown-menu').show();
-            $('#solution-menu').hide();
-            $('#product-dropdown-menu').hide();
-        });
-        $('#dropdown-menu').bind("mouseleave", null, function () {
-        		$('#dropdown-menu').hide();
-        });
-
-         $('#solution').on("mouseover", function () {
-            $('#solution-menu').show();
-            $('#dropdown-menu').hide();
-            $('#product-dropdown-menu').hide();
-        });
-		
-        $('#solution-menu').bind("mouseleave", null, function () {
-        		$('#solution-menu').hide();
-        });
-
-        $('#product-dropdown').on("mouseover", function () {
-            $('#product-dropdown-menu').show();
-            $('#dropdown-menu').hide();
-            $('#solution-menu').hide();
-        });
-        
-        $('#product-dropdown-menu').bind("mouseleave", null, function () {
+        if (isPC()) {
+            $('#dropdown').on("mouseover", function () {
+                $('#dropdown-menu').show();
+                $('#solution-menu').hide();
                 $('#product-dropdown-menu').hide();
-        });
+                $('#enterprise-menu').hide();
+            });
+            $('#dropdown-menu').bind("mouseleave", null, function () {
+                $('#dropdown-menu').hide();
+            });
+
+            $('#solution').on("mouseover", function () {
+                $('#solution-menu').show();
+                $('#dropdown-menu').hide();
+                $('#product-dropdown-menu').hide();
+            });
+
+            $('#solution-menu').bind("mouseleave", null, function () {
+                $('#solution-menu').hide();
+            });
+
+            $('#enterprise').on("mouseover", function () {
+                $('#enterprise-menu').show();
+                $('#dropdown-menu').hide();
+                $('#product-dropdown-menu').hide();
+            });
+
+            // $('#enterprise').on("mouseleave", function () {
+            //     $('#enterprise-menu').hide();
+            // });
+
+            $('#enterprise-menu').bind("mouseleave", null, function () {
+                $('#enterprise-menu').hide();
+            });
+
+            $('#product-dropdown').on("mouseover", function () {
+                $('#product-dropdown-menu').show();
+                $('#dropdown-menu').hide();
+                $('#solution-menu').hide();
+            });
+
+            $('#product-dropdown-menu').bind("mouseleave", null, function () {
+                $('#product-dropdown-menu').hide();
+            });
 
 
-
-        $('.nav > li').not(".dropdown-solution").bind("mouseover", null, function () {
-            $('.solution-menu').hide();
-            if($(document).scrollTop() <= 0){
-                $('nav').removeClass("nav-scroll")
-            }
-        });
+            $('.nav > li').not(".dropdown-solution").bind("mouseover", null, function () {
+                $('.solution-menu').hide();
+                if ($(document).scrollTop() <= 0) {
+                    $('nav').removeClass("nav-scroll")
+                }
+            });
         }
 
         // nav scroll
@@ -86,7 +100,7 @@
                 $("#nav").addClass("nav-scroll");
             }
         });
-        
+
         // footer
         $(function () {
             var image = '<img src="pics/qr-winxin.png">';
@@ -104,55 +118,56 @@
         });
     })
 })(window);
-function bindEvent(){
-	var w=window.innerWidth;
-	var flag=false; 
-	$('#dropdown-click').on('click',function(){
-		if(w<769 && flag==false){
-			$('#dropdown').css({
-				'height':'230px'
-			})
-			$('#dropdown-menu').show()
-			flag=true;
-		}else if(w<769 && flag==true){
-			$('#dropdown').css({
-				'height':'38px'
-			})
-			$('#dropdown-menu').hide()
-			flag=false;
-		}
-	})
-	$('.dropdown-about').on('click',function(){
-		if(w<769 && flag==false){
-			$('#solution').css({
-				'height':'180px'
-			})
-			$('.solution-menu').show()
-			flag=true;
-		}else if(w<769 && flag==true){
-			$('#solution').css({
-				'height':'38px'
-			})
-			$('.solution-menu').hide()
-			flag=false;
-		}
-	}) 
-	
+
+function bindEvent() {
+    var w = window.innerWidth;
+    var flag = false;
+    $('#dropdown-click').on('click', function () {
+        if (w < 769 && flag == false) {
+            $('#dropdown').css({
+                'height': '230px'
+            })
+            $('#dropdown-menu').show()
+            flag = true;
+        } else if (w < 769 && flag == true) {
+            $('#dropdown').css({
+                'height': '38px'
+            })
+            $('#dropdown-menu').hide()
+            flag = false;
+        }
+    })
+    $('.dropdown-about').on('click', function () {
+        if (w < 769 && flag == false) {
+            $('#solution').css({
+                'height': '180px'
+            })
+            $('.solution-menu').show()
+            flag = true;
+        } else if (w < 769 && flag == true) {
+            $('#solution').css({
+                'height': '38px'
+            })
+            $('.solution-menu').hide()
+            flag = false;
+        }
+    })
+
 }
 
-if(!isPC()) {
-	bindEvent();
-$(window).resize(function() {
- 	bindEvent();
-});
+if (!isPC()) {
+    bindEvent();
+    $(window).resize(function () {
+        bindEvent();
+    });
 }
 
 function isPC() {
     var userAgentInfo = navigator.userAgent;
-    	
+
     var Agents = ["Android", "iPhone",
-                "SymbianOS", "Windows Phone",
-                "iPad", "iPod"];
+        "SymbianOS", "Windows Phone",
+        "iPad", "iPod"];
     var flag = true;
     for (var v = 0; v < Agents.length; v++) {
         if (userAgentInfo.indexOf(Agents[v]) > 0) {
